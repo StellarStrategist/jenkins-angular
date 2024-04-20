82e096471b69d4d9e5eb4e77e07614bd5fce8317
Niharika <niharikatiwari023@gmail.com>
2024-04-20 14:17:13 +0530

    Updated Jenkinsfile

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-ng build --configuration production
-ng serve --configuration production &
+npm run ng build
+npm run ng serve &
 sleep 1
 echo $! > .pidfile
 
 echo 'Now...'
-echo 'Visit http://localhost:4200 to see your Node.js/React application in action.'
+echo 'Visit http://localhost:4200 to see your Node.js/Angular application in action.'
